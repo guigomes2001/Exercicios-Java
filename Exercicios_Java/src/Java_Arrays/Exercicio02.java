08024a93de9b3f11e04a4e13ebca5230c26be2ed
GuiKabulozu <guilhermegomesf4@gmail.com>
2024-05-27 07:00:37 -0300

    Exercício em Java - Programa básico com utilização de Array

--- Exercicios_Java/src/Java_Arrays/Exercicio02.java
+++ Exercicios_Java/src/Java_Arrays/Exercicio02.java
@@ -3,6 +3,7 @@ package Java_Arrays;
 public class Exercicio02 {
     public static void main(String[] args) {
     	
+    	//Programa básico com utilização de Array
     	
         int[] vetorA = {1, 2, 3, 4, 5, 6, 7, 8};
         int[] vetorB = new int[vetorA.length]; 
