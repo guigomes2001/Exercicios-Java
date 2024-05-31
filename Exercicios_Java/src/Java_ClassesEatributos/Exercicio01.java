865810e1fcb446d89efddbc8ae6ad9dd6d7029f2
GuiKabulozu <guilhermegomesf4@gmail.com>
2024-05-31 17:36:54 -0300

    Exercício em Java - Programa básico de objeto representando informaçẽs de um lâmpada

---
+++ Exercicios_Java/src/Java_ClassesEatributos/Exercicio01.java
@@ -0,0 +1,33 @@
+package Java_ClassesEatributos;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		
+		//Programa básico de objeto representando informaçẽs de um lâmpada 
+		
+		Lampada objeto = new Lampada();
+		
+		objeto.tipo = "Led";
+		objeto.cor = "Transparente";
+		objeto.marca = "Philips";
+		objeto.valor = 10;
+		
+		System.out.println("O tipo da lâmpada é " + objeto.tipo);
+		System.out.println("A cor da lâmpada é " + objeto.cor);
+		System.out.println("A marca da lâmpada é " + objeto.marca);
+		System.out.println("O valor da lâmpada é " + objeto.valor);
+		
+	}
+	
+	 static class Lampada {
+			
+			String tipo;
+			String cor;
+			String marca;
+			byte valor;
+
+	 }
+
+}
+
